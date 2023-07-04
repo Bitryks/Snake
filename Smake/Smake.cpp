48a09c0981c3925ac12a2d1cb569d5c868893def
Bitryks <128896462+Bitryks@users.noreply.github.com>
2023-07-04 11:18:51 +0300

    Devel_Moment

--- Smake/Smake.cpp
+++ Smake/Smake.cpp
@@ -1,12 +1,39 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
+bool gameOver;
+const int witdh = 20;
+const int height = 20;
+int x, t, fruitX, fruitY, score;
+
+
 void setup()
+{
+    gameOver = false;
+}
+
+void draw()
+{
+
+}
+
+void input() 
+{
+
+}
+
+void Logic()
 {
 
 }
 int main()
 {
+    setup();
+    while (!gameOver)
+    {
+        draw();
+        input();
+        Logic();
+    }
     return 0;
 }
-
