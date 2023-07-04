2c707243c04cc234dbc0bdb23e76453fed220270
Bitryks <128896462+Bitryks@users.noreply.github.com>
2023-07-04 12:10:26 +0300

    creating a field of play

--- Smake/Smake.cpp
+++ Smake/Smake.cpp
@@ -2,19 +2,48 @@
 using namespace std;
 
 bool gameOver;
-const int witdh = 20;
+const int width = 20;
 const int height = 20;
 int x, t, fruitX, fruitY, score;
-
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN  };
+eDirection dir;
 
 void setup()
 {
     gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitX = rand() % height;
+    score = 0;
+
+
 }
 
 void draw()
 {
+    system("cls");
+    for(int i = 0; i < width; i++)
+    
+        cout << "#";
+        cout << endl;
 
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++) {
+                if (j == 0 || j == width - 1)
+                    cout << "#";
+                cout << " ";
+        }
+            cout << endl;
+    }
+
+    for (int i = 0; i < width; i++)
+
+            cout << "#";
+        cout << endl;
+    
 }
 
 void input() 
@@ -37,3 +66,4 @@ int main()
     }
     return 0;
 }
+
